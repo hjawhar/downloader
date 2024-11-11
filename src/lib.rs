772a395461e7f617201a903af8ed26a95c1b4365
Hassan Jawhar <hassanjawhar@protonmail.com>
2024-11-11 09:32:10 +0300

    Added create downloads folder + fetch file name and extension based on url path

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,7 @@ use futures::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
 use tokio::{fs::File, io::AsyncWriteExt};
 
-pub async fn download_files(
-    url: &str,
-    path: &str,
-) -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
+pub async fn download_files(url: &str) -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
     let pb = ProgressBar::new(0);
     let mut downloaded = 0;
     pb.set_style(
@@ -16,7 +13,21 @@ pub async fn download_files(
         .progress_chars("#>-"),
     );
 
-    let mut file = File::create(path).await?;
+    let file_name = url.split("/").last().unwrap_or("Unknown");
+    match std::fs::exists("./downloads") {
+        Ok(exists) => {
+            if !exists {
+                let created_dir = std::fs::create_dir("./downloads");
+                if let Err(err) = created_dir {
+                    panic!("Failed to create downloads directory {:#?}", err);
+                }
+            }
+        }
+        Err(err) => {
+            panic!("Couldn't resolve downloads folder {:#?}", err);
+        }
+    }
+    let mut file = File::create(format!("./downloads/{}", file_name)).await?;
     println!("Downloading {}...", url);
     let mut stream = reqwest::get(url).await?.bytes_stream();
     while let Some(chunk_result) = stream.next().await {
@@ -38,7 +49,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_download() {
-        let test_dl = download_files("https://ash-speed.hetzner.com/100MB.bin", "100MB.bin").await;
+        let test_dl = download_files("https://ash-speed.hetzner.com/100MB.bin").await;
         match test_dl {
             Ok(res) => assert_eq!(res, true),
             Err(err) => panic!("Something went wrong {:#?}", err),
